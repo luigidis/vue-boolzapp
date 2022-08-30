a6d5692c35e65fb59c48505cf19272ebcfd9cfc5
LuigiDiS <luigi.distefano69@gmail.com>
2022-08-30 18:59:08 +0200

    Continuo Milestone 5

--- js/app.js
+++ js/app.js
@@ -9,17 +9,20 @@ contacts = [
     {
     date: '10/01/2020 15:30:55',
     message: 'Hai portato a spasso il cane?',
-    status: 'sent'
+    status: 'sent',
+    option: 'not-visible',
     },
     {
     date: '10/01/2020 15:50:00',
     message: 'Ricordati di stendere i panni',
-    status: 'sent'
+    status: 'sent',
+    option: 'not-visible',
     },
     {
     date: '10/01/2020 16:15:22',
     message: 'Tutto fatto!',
-    status: 'received'
+    status: 'received',
+    option: 'not-visible',
     }
     ],
     },
@@ -31,17 +34,20 @@ contacts = [
     {
     date: '20/03/2020 16:30:00',
     message: 'Ciao come stai?',
-    status: 'sent'
+    status: 'sent',
+    option: 'not-visible',
     },
     {
     date: '20/03/2020 16:30:55',
     message: 'Bene grazie! Stasera ci vediamo?',
-    status: 'received'
+    status: 'received',
+    option: 'not-visible',
     },
     {
     date: '20/03/2020 16:35:00',
     message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-    status: 'sent'
+    status: 'sent',
+    option: 'not-visible',
     }
     ],
     },
@@ -53,17 +59,20 @@ contacts = [
     {
     date: '28/03/2020 10:10:40',
     message: 'La Marianna va in campagna',
-    status: 'received'
+    status: 'received',
+    option: 'not-visible',
     },
     {
     date: '28/03/2020 10:20:10',
     message: 'Sicuro di non aver sbagliato chat?',
-    status: 'sent'
+    status: 'sent',
+    option: 'not-visible',
     },
     {
     date: '28/03/2020 16:15:22',
     message: 'Ah scusa!',
-    status: 'received'
+    status: 'received',
+    option: 'not-visible',
     }
     ],
     },
@@ -75,12 +84,14 @@ contacts = [
     {
     date: '10/01/2020 15:30:55',
     message: 'Lo sai che ha aperto una nuova pizzeria?',
-    status: 'sent'
+    status: 'sent',
+    option: 'not-visible',
     },
     {
     date: '10/01/2020 15:50:00',
     message: 'Si, ma preferirei andare al cinema',
-    status: 'received'
+    status: 'received',
+    option: 'not-visible',
     }
     ],
     },
@@ -92,12 +103,14 @@ contacts = [
     {
     date: '10/01/2020 15:30:55',
     message: 'Ricordati di chiamare la nonna',
-    status: 'sent'
+    status: 'sent',
+    option: 'not-visible',
     },
     {
     date: '10/01/2020 15:50:00',
     message: 'Va bene, stasera la sento',
-    status: 'received'
+    status: 'received',
+    option: 'not-visible',
     }
     ],
     },
@@ -109,17 +122,20 @@ contacts = [
     {
     date: '10/01/2020 15:30:55',
     message: 'Ciao Claudia, hai novità?',
-    status: 'sent'
+    status: 'sent',
+    option: 'not-visible',
     },
     {
     date: '10/01/2020 15:50:00',
     message: 'Non ancora',
-    status: 'received'
+    status: 'received',
+    option: 'not-visible',
     },
     {
     date: '10/01/2020 15:51:00',
     message: 'Nessuna nuova, buona nuova',
-    status: 'sent'
+    status: 'sent',
+    option: 'not-visible',
     }
     ],
     },
@@ -131,12 +147,14 @@ contacts = [
     {
     date: '10/01/2020 15:30:55',
     message: 'Fai gli auguri a Martina che è il suo compleanno!',
-    status: 'sent'
+    status: 'sent',
+    option: 'not-visible',
     },
     {
     date: '10/01/2020 15:50:00',
     message: 'Grazie per avermelo ricordato, le scrivo subito!',
-    status: 'received'
+    status: 'received',
+    option: 'not-visible',
     }
     ],
     },
@@ -148,21 +166,24 @@ contacts = [
     {
     date: '10/01/2020 15:30:55',
     message: 'Ciao, andiamo a mangiare la pizza stasera?',
-    status: 'received'
+    status: 'received',
+    option: 'not-visible',
     },
     {
     date: '10/01/2020 15:50:00',
     message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-    status: 'sent'
+    status: 'sent',
+    option: 'not-visible',
     },
     {
     date: '10/01/2020 15:51:00',
     message: 'OK!!',
-    status: 'received'
+    status: 'received',
+    option: 'not-visible',
     }
     ],
     }
-    ]
+]
 
     const app = new Vue ({
         el: '#app',
@@ -172,7 +193,8 @@ contacts = [
             filter: '',
             messageToPush: '',
             aVis: false,
-            isSearching: false
+            isSearching: false,
+            
         },
             
             
@@ -182,12 +204,13 @@ contacts = [
             },
             sentMessagePush() {
                 cleanedText = this.messageToPush.trim()
-                if (cleanedText === '') return
+               if (cleanedText === '') return
                 messages = this.contacts[this.currentContact].messages
                 message = {
                     date: '',
                     message: cleanedText,
-                    status: 'sent'
+                    status: 'sent',
+                    option: 'not-visible',
                 }
                 messages.push(message)
                 this.messageToPush = ''
@@ -196,11 +219,18 @@ contacts = [
                         date: '',
                         message: 'Ok!',
                         status: 'received',
+                        option: 'not-visible',
                     }
                     messages.push(messageOk)
                 },2000)
+            },
+            hide (index) {
+                // console.log('click on hide')
+                if (this.contacts[this.currentContact].messages[index].option === 'visible') {
+                    this.contacts[this.currentContact].messages[index].option = 'not-visible'
+                }
+                 else {this.contacts[this.currentContact].messages[index].option = 'visible'}
             }
-            
         },
     })
                     
